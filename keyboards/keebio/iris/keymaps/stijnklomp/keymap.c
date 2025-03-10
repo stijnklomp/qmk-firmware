6d21bec08e8ca18e7c225ba57f8e06f291da2827
Stijn Klomp <sklomp@friendmts.com>
2025-03-10 00:42:17 +0000

    macOS layout corrected

--- keyboards/keebio/iris/keymaps/stijnklomp/keymap.c
+++ keyboards/keebio/iris/keymaps/stijnklomp/keymap.c
@@ -66,6 +66,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 // tap_code(KC_LALT);
                 register_code(KC_LALT);
             } else {
+                if (get_highest_layer(layer_state) == LMAC0 || get_highest_layer(layer_state) == LMAC1) {
+                    // if (left_tab_pressed) {
+                        unregister_code(KC_LGUI);
+                        if (left_shift_pressed) {
+                            unregister_code(KC_LSFT);
+                        }
+                    // }
+                    // left_tab_pressed = false;
+                }
                 left_alt_pressed = false;
                 unregister_code(KC_LALT);
             }
@@ -75,7 +84,36 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case KC_THREE: // KC_TAB
             if (record->event.pressed) {
                 left_tab_pressed = true;
-                tap_code(KC_TAB);
+                if (get_highest_layer(layer_state) == LMAC0 || get_highest_layer(layer_state) == LMAC1) {
+                    if (left_gui_pressed) {
+                        unregister_code(KC_LGUI);
+                        if (left_shift_pressed) {
+                            tap_code16(S(LCTL(KC_TAB)));
+                        } else {
+                            tap_code16(LCTL(KC_TAB));
+                        }
+                        register_code(KC_LGUI);
+                    } else if (left_alt_pressed) {
+                        unregister_code(KC_LALT);
+                        if (left_shift_pressed) {
+                            // tap_code16(S(LGUI(KC_TAB)));
+                            register_code(KC_LSFT);
+                            register_code(KC_LGUI);
+                            tap_code(KC_TAB);
+                            // unregister_code(KC_LGUI);
+                        } else {
+                            // tap_code16(LGUI(KC_TAB));
+                            register_code(KC_LGUI);
+                            tap_code(KC_TAB);
+                            // unregister_code(KC_LGUI);
+                        }
+                        register_code(KC_LALT);
+                    } else {
+                        tap_code(KC_TAB);
+                    }
+                } else {
+                    tap_code(KC_TAB);
+                }
                 // if (left_control_pressed) {
                 //     // unregister_code(KC_LCTL);
                 //     // unregister_code(KC_LALT);
@@ -95,7 +133,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 //     tap_code(KC_TAB);
                 // }
             } else {
-                left_tab_pressed = false;
+                // if (get_highest_layer(layer_state) == LMAC0 || get_highest_layer(layer_state) == LMAC1) {
+                //     if (left_alt_pressed) {
+                //         unregister_code(KC_LGUI);
+                //         if (left_shift_pressed) {
+                //             unregister_code(KC_LSFT);
+                //         }
+                //     }
+                //     left_tab_pressed = false;
+                // } else {
+                    left_tab_pressed = false;
+                // }
             }
             return false;
 
@@ -233,14 +281,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [LMAC0] = LAYOUT(
         KC_ESC     , KC_1       , KC_2       , KC_3      , KC_4      , KC_5      ,                         KC_6      , KC_7      , KC_8      , KC_9      , KC_0      , KC_MEDIA_PLAY_PAUSE ,
-        KC_TAB     , KC_Q       , KC_W       , KC_E      , KC_R      , KC_T      ,                         KC_Y      , KC_U      , KC_I      , KC_O      , KC_P      , KC_BSPC   ,
+        KC_THREE   , KC_Q       , KC_W       , KC_E      , KC_R      , KC_T      ,                         KC_Y      , KC_U      , KC_I      , KC_O      , KC_P      , KC_BSPC   ,
         MO(LMAC1)  , KC_A       , KC_S       , KC_D      , KC_F      , KC_G      ,                         KC_H      , KC_J      , KC_K      , KC_L      , KC_SCLN   , MO(LMAC1) ,
         KC_LSFT    , KC_Z       , KC_X       , KC_C      , KC_V      , KC_B      , KC_VOLD   , KC_VOLU   , KC_N      , KC_M      , KC_COMM   , KC_DOT    , KC_SLSH   , KC_LSFT   ,
                                                            KC_SEVEN  , KC_TWO    , KC_SPC    , KC_ENT    , KC_LSFT   , KC_SEVEN
     ),
     [LMAC1] = LAYOUT(
         KC_NO      , KC_F1      , KC_F2      , KC_F3     , KC_F4     , KC_F5     ,                         KC_F6     , KC_F7     , KC_F8     , KC_F9     , KC_F10    , KC_F11    ,
-        KC_TAB     , KC_EXLM    , KC_AT      , KC_CIRC   , KC_DLR    , KC_PERC   ,                         KC_PIPE   , KC_LBRC   , KC_RBRC   , KC_QUOT   , KC_PPLS   , KC_F12    ,
+        KC_THREE   , KC_EXLM    , KC_AT      , KC_CIRC   , KC_DLR    , KC_PERC   ,                         KC_PIPE   , KC_LBRC   , KC_RBRC   , KC_QUOT   , KC_PPLS   , KC_F12    ,
         KC_DEL     , KC_TILD    , KC_HASH    , KC_AMPR   , KC_LPRN   , KC_RPRN   ,                         KC_FIVE   , KC_DOWN   , KC_UP     , KC_SIX    , KC_PMNS   , KC_NO     ,
         KC_LSFT    , KC_QUES    , M_BTICK    , KC_ASTR   , KC_UNDS   , KC_F11    , TO(LSPEC0), TO(LSPEC0), KC_F12    , KC_PEQL   , KC_LT     , KC_GT     , KC_NO     , KC_LSFT   ,
                                                            KC_SEVEN  , KC_TWO    , KC_SPC    , KC_ESC    , KC_LSFT   , KC_SEVEN
