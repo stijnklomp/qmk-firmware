72513f70657912152d0d0e80aebc5c32a58fb9c3
Stijn Klomp <sklomp@friendmts.com>
2025-04-30 11:36:32 +0100

    macOS fixes

--- keyboards/keebio/iris/keymaps/stijnklomp/keymap.c
+++ keyboards/keebio/iris/keymaps/stijnklomp/keymap.c
@@ -405,12 +405,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case CK_C: // c
             if (record->event.pressed) {
                 c_pressed = true;
-                if (is_linux_layer(layer_state)) {
-                    if (right_control_pressed) {
-                        // Use "left control" + "shift" + "c" to return SIGINT in Kitty terminal
-                        tap_code16(S(LCTL(KC_C)));
-                        return false;
-                    }
+                if (right_control_pressed) {
+                    // Use "left control" + "shift" + "c" to return SIGINT in Kitty terminal
+                    tap_code16(S(LCTL(KC_C)));
+                    return false;
                 }
 
                 tap_code(KC_C);
@@ -428,8 +426,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                         unregister_code(KC_LGUI);
                         register_code(KC_LALT);
                         tap_code(KC_BSPC);
-                        register_code(KC_LGUI);
                         unregister_code(KC_LALT);
+                        register_code(KC_LGUI);
                         return false;
                     }
 
@@ -438,8 +436,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                         unregister_code(KC_LALT);
                         register_code(KC_LGUI);
                         tap_code(KC_BSPC);
-                        register_code(KC_LALT);
                         unregister_code(KC_LGUI);
+                        register_code(KC_LALT);
                         return false;
                     }
                 }
@@ -465,7 +463,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC     , KC_1       , KC_2       , KC_3      , KC_4      , KC_5      ,                         KC_6      , KC_7      , KC_8      , KC_9      , KC_0      , KC_MEDIA_PLAY_PAUSE ,
         CK_TAB     , KC_Q       , KC_W       , KC_E      , KC_R      , KC_T      ,                         KC_Y      , KC_U      , KC_I      , KC_O      , KC_P      , KC_BSPC   ,
         MO(LWIN1)  , KC_A       , KC_S       , KC_D      , KC_F      , KC_G      ,                         KC_H      , KC_J      , KC_K      , KC_L      , KC_SCLN   , MO(LWIN1) ,
-        CK_LSFT    , KC_Z       , KC_X       , KC_C      , KC_V      , KC_B      , KC_VOLD   , KC_VOLU   , KC_N      , KC_M      , KC_COMM   , KC_DOT    , KC_SLSH   , CK_LSFT   ,
+        CK_LSFT    , KC_Z       , KC_X       , CK_C      , KC_V      , KC_B      , KC_VOLD   , KC_VOLU   , KC_N      , KC_M      , KC_COMM   , KC_DOT    , KC_SLSH   , CK_LSFT   ,
                                                            CK_LCTL   , CK_LALT   , KC_SPC    , KC_ENT    , CK_LSFT   , CK_RCTL
     ),
     [LWIN1] = LAYOUT(
@@ -479,7 +477,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC     , KC_1       , KC_2       , KC_3      , KC_4      , KC_5      ,                         KC_6      , KC_7      , KC_8      , KC_9      , KC_0      , KC_MEDIA_PLAY_PAUSE ,
         CK_TAB     , KC_Q       , KC_W       , KC_E      , KC_R      , KC_T      ,                         KC_Y      , KC_U      , KC_I      , KC_O      , KC_P      , CK_BSPC   ,
         MO(LMAC1)  , KC_A       , KC_S       , KC_D      , KC_F      , KC_G      ,                         KC_H      , KC_J      , KC_K      , KC_L      , KC_SCLN   , MO(LMAC1) ,
-        CK_LSFT    , KC_Z       , KC_X       , KC_C      , KC_V      , KC_B      , KC_VOLD   , KC_VOLU   , KC_N      , KC_M      , KC_COMM   , KC_DOT    , KC_SLSH   , CK_LSFT   ,
+        CK_LSFT    , KC_Z       , KC_X       , CK_C      , KC_V      , KC_B      , KC_VOLD   , KC_VOLU   , KC_N      , KC_M      , KC_COMM   , KC_DOT    , KC_SLSH   , CK_LSFT   ,
                                                            CK_LGUI   , CK_LALT   , KC_SPC    , KC_ENT    , CK_LSFT   , CK_RCTL
     ),
     [LMAC1] = LAYOUT(
